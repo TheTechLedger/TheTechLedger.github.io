a94b78723a25cd235441052c5608e3ed8dfd2cf9
theAlphaGuardian <joseph.rasanjana1@gmail.com>
2025-07-05 23:52:31 +0530

    minor

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const repo = 'TheTechLedger.github.io';
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
+  basePath: `${repo}`,
+  assetPrefix: `${repo}/`,
   images: {
     unoptimized: true,
   },
